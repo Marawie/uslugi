a8f588609673cdb8f11a117d40d9a0ae33b81386
MarekMarczak <marawieeee@gmail.com>
2024-08-04 19:06:42 +0200

    git init

---
+++ src/test/java/Uslugi/uslugi/UslugiApplicationTests.java
@@ -0,0 +1,13 @@
+package Uslugi.uslugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UslugiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
